669a67e67d9225dedf8f5f8bd68945384d50ffb4
abdallah <abdallah.bouhannache@gmail.com>
2023-09-18 02:24:59 +0100

    finished my first working version of backedn ,where can download multiple times ,also can  stop the downloading ,but thhe resume still not functioning ,also when stopped ,if resend request of a new download with same file name it will crash

--- src/server/app.py
+++ src/server/app.py
@@ -1,11 +1,16 @@
+import json
 import aiohttp
 import asyncio
 from tool import get_file_size
 from flask import Flask, request, jsonify
 import os
-
+import re
+import requests
 
 app = Flask(__name__)
+
+downloads = {}
+
 
 @app.route('/download_file', methods=['POST'])
 async def download_file():
@@ -14,7 +19,7 @@ async def download_file():
     save_dir = data.get('path') or None
     file_name = data.get('filename') or None
     speed_limit = data.get('speed') or None
-
+    save_state_file = data.get('download_state') or os.path.join(save_dir, 'download_state.json')
     # # URL of the file you want to download
     # file_url = 'https://example.com/file_url'
 
@@ -24,74 +29,150 @@ async def download_file():
     # # Speed limit in bytes per second
     # speed_limit = 1024  # 1KB/s
 
+
+    # byte_range = 0
+    # # Load the start byte from the state file, if it exists
+    # if os.path.exists(save_state_file):
+    #     with open(save_state_file, 'r') as state_file:
+    #         state = json.load(state_file)
+    #     byte_range = int(state['last_byte'])
+
+    # save_state_file = not save_state_file and os.path.join(save_dir, 'download_state.json')
+
+
+    file_name = os.path.basename(file_url) if not file_name else "download"
+    # Get the filename from the response headers
+    if not file_name :
+        res = requests.get(file_url)
+        content_disposition = res.headers.get('Content-Disposition')
+        file_name = get_filename_from_content_disposition(content_disposition)
+
+
+    global downloads
+    # Load the state from the save_state_file, if it exists
+    if os.path.exists(save_state_file):
+        with open(save_state_file, 'r') as state_file:
+                downloads = json.load(state_file) 
+
+
+     # Create a unique state entry for each download
+    if file_name not in downloads:
+        downloads[file_name] = {'last_byte': 0, 'running': True}
+    byte_range = downloads[file_name]['last_byte']
+    # Construct the headers using the byte range
+    headers = {'Range': 'bytes={}-'.format(byte_range)}
+    print(downloads,"at begingin")
     async with aiohttp.ClientSession() as session:
-        async with session.get(file_url) as response:
+        async with session.get(file_url,headers=headers) as response:
 
-            file_name = os.path.basename(file_url) if not file_name else ""
-            # Get the filename from the response headers
-            if not file_name :
-                content_disposition = response.headers.get('Content-Disposition')
-                file_name = get_filename_from_content_disposition(content_disposition)
-            
-            [file_name,ext]=file_name.split('.')
-            
-            print(file_name,ext)
-            content_dir=os.listdir(save_dir)
-            file_count=content_dir.count(file_name)
+            # file_name = os.path.basename(file_url) if not file_name else ""
+            # # Get the filename from the response headers
+            # if not file_name :
+            #     content_disposition = response.headers.get('Content-Disposition')
+            #     file_name = get_filename_from_content_disposition(content_disposition)
+           
+            if  byte_range:
+                [file_name,ext]=file_name.split('.')
+                pattern = re.compile(f'^{re.escape(file_name)}(\(\d+\))?.{re.escape(ext)}$')
+                content_dir=os.listdir(save_dir)
+                # print(content_dir)
+                # file_count=content_dir.count(pattern)
+                matching_files = [f for f in content_dir if pattern.match(f)]
+                file_count = len(matching_files)
+                # print(file_name,ext)
+                # print(f'file_count {file_count}')
+                file_name=f'{file_name}({file_count+1}).{ext}'
 
-            if file_count:
-                file_name=f'{file_name}({file_count}).{ext}'
-
-            print(f'filename={file_name}')
-            return
-
+            # print(f'tobesaved_as_file_name={file_name}')
             save_path = os.path.join(save_dir, file_name)
-            print(file_name)
+            # print(file_name)
             # Get the file size from the response headers
             file_size = int(response.headers.get('Content-Length'))
-            print(get_file_size(file_size))
-
+            # print(get_file_size(file_size))
             # Calculate the number of chunks based on the speed limit
             # chunk_size = min(speed_limit, 1024*1024)
             chunk_size=500 * 1024 
             num_chunks = file_size // chunk_size
-            print(f'chunks {num_chunks} with chunk size  {get_file_size(chunk_size)} ')
-
+            # print(f'chunks {num_chunks} with chunk size  {get_file_size(chunk_size)} ')
             # Create a list to store the tasks for parallel downloading
             tasks = []
-
             file_size_on_disk = 0
-
             # Download the file in chunks asynchronously
             # for i in range(num_chunks):
             while file_size_on_disk < file_size :
+                # # Check if the download has been stopped
+                # if file_name in downloads and not downloads[file_name]['running']:
+                #     break
+                # Check if the download has been stopped
+                if not downloads[file_name]['running']:
+                    break
+                
                 # Read a chunk of data from the response
-                data = await response.content.read(256000)
+                data = await response.content.read(256)
+
+                # if not data:
+                #     # Download is complete  
+                #     downloads.pop(file_name)
+                #     # if os.path.exists(save_state_file):
+                #     #     os.remove(save_state_file)
+                #     print("exited cause file downloading finish ,removed from download_state.json")
+                #     break
+
+                # Update the byte range
+                byte_range += len(data)
+
                 # print(f'downloaded :--->{get_file_size(chunk_size)}')
                 print(f'downloading {get_file_size(len(data))}')
                 # Create a task to write the chunk to the file
-                task = asyncio.create_task(write_chunk_to_file(save_path, data))
+                task = asyncio.create_task(write_chunk_to_file(save_path,data,file_name,save_state_file,file_size_on_disk))
                 tasks.append(task)
+
+                # task = asyncio.ensure_future(write_chunk_to_file(save_path,data))
+                # task.add_done_callback(coroutine())
+                # tasks.append(task)
 
                 # Delay between chunks to limit the download speed
                 await asyncio.sleep(1)
-                file_size_on_disk=os.path.getsize(save_path)
+                # file_size_on_disk=os.path.getsize(save_path)
 
             # Download the remaining bytes
-            remaining_bytes = file_size % chunk_size
-            data = await response.content.read(remaining_bytes)
-            task = asyncio.create_task(write_chunk_to_file(save_path, data))
-            tasks.append(task)
+            # remaining_bytes = file_size % chunk_size
+            # data = await response.content.read(remaining_bytes)
+            # task = asyncio.create_task(write_chunk_to_file(save_path,data,file_name,save_state_file,file_size_on_disk))
+            # tasks.append(task)
 
             # Wait for all tasks to complete
             await asyncio.gather(*tasks)
 
     return "File downloaded successfully"
 
-async def write_chunk_to_file(file_path, data):
+
+
+async def write_chunk_to_file(file_path, data, file_name,save_state_file,file_size_on_disk):
     print(f'appending {get_file_size(len(data))}')
     with open(file_path, 'ab') as file:
         file.write(data)
+    #  # Save the current state to the save_state_file
+    # with open(save_state_file, 'w') as state_file:
+    #     json.dump(downloads, state_file)
+   # Save the current state to the save_state_file
+
+    if downloads[file_name]:
+        print("stored file")
+        with open(save_state_file, 'a') as state_file:
+            json.dump({file_name: downloads[file_name]}, state_file)
+            state_file.write('\n')
+            downloads[file_name]
+        file_size_on_disk=os.path.getsize(file_path)
+        return file_size_on_disk
+
+
+# def coroutine(file_path,file_name,save_state_file,file_size_on_disk):
+#     with open(save_state_file, 'a') as state_file:
+#         json.dump({file_name: downloads[file_name]}, state_file)
+#         state_file.write('\n')
+#     file_size_on_disk=os.path.getsize(file_path)
+    
 
 def get_filename_from_content_disposition(content_disposition):
     if content_disposition:
@@ -101,6 +182,34 @@ def get_filename_from_content_disposition(content_disposition):
                 filename = part.split('=')[1].strip().strip('"')
                 return filename
     return 'downloaded_file'
+
+
+@app.route('/stop_download', methods=['POST'])
+def stop_download():
+    data = request.get_json()
+    file_name = data.get('filename')
+
+    if file_name in downloads:
+        downloads[file_name]['running'] = False
+
+    return "Download stopped successfully"
+
+
+
+@app.route('/resume_download', methods=['POST'])
+def resume_download():
+    data = request.get_json()
+    file_name = data.get('filename')
+
+    if file_name in downloads:
+        downloads[file_name]['running'] = True
+    else:
+        downloads[file_name] = {'running': True}
+
+        # Start a new download task
+        asyncio.create_task(download_file())
+
+    return "Download resumed successfully"
 
 if __name__ == '__main__':
     app.run()
